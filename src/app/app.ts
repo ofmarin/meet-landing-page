f1d735fb9f29ee54045d551930d154b56892f0b1
ofmarin <ofelmaran@gmail.com>
2026-06-18 20:16:05 -0400

    fixed mobile, todo: finish tablet fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Nav} from './nav/nav';
-import {Btn} from './btn/btn';
 import {Hero} from './hero/hero';
 import {Footer} from './footer/footer';
 import {MainContent} from './main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav, Btn, Hero, Footer, MainContent],
+  imports: [RouterOutlet, Nav, Hero, Footer, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
